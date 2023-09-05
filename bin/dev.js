d11de7abcb2c5f38a45cee1e9c79cb88ab9244fb
Bader Nasser <bader4tech@gmail.com>
2023-09-06 03:19:42 +0400

    Add code from upstream: oclif/hello-world-esm (1ed6a17)

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,4 @@
-#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
+#!/usr/bin/env node
 
-(async () => {
-	const oclif = await import('@oclif/core');
-	await oclif.execute({type: 'esm', development: true, dir: import.meta.url});
-})();
+const oclif = await import('@oclif/core');
+await oclif.execute({development: true, dir: import.meta.url});
--- bin/run.js
+++ bin/run.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 
-(async () => {
-	const oclif = await import('@oclif/core');
-	await oclif.execute({type: 'esm', dir: import.meta.url});
-})();
+const oclif = await import('@oclif/core');
+await oclif.execute({dir: import.meta.url});
