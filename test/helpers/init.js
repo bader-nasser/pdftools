adeaeb1d1e77c1a2dd9ff160ca901be5a567e82e
Bader Nasser <bader4tech@gmail.com>
2023-11-18 18:37:29 +0400

    Remove useless files

--- test/helpers/init.js
+++
@@ -1,8 +0,0 @@
-import path from 'node:path';
-import process from 'node:process';
-
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
-process.env.NODE_ENV = 'development';
-
-global.oclif = global.oclif || {};
-global.oclif.columns = 80;
